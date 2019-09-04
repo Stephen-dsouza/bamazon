fee3159a0028ca81ba26cfc72c832ca59f3e4bf5
Stephen <stephen.roy.dsouza@gmail.com>
2019-09-04 19:29:52 +1000

    adding function to display and prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,18 +1,47 @@
+
+//for .env to save sql keys
 require("dotenv").config();
 var keys = require("./keys.js");
+//for inquirer
 var inquirer = require("inquirer");
+//for mysql connection and npm package
 var mysql = require("mysql");
 var connection=mysql.createConnection((keys.sql));
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "srds1221",
-//     database: "bamazon"
-//   });
+
 
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    
+    customerPurchase();
 });
+
+function customerPurchase(){
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.table(res);
+    
+  });
+
+  inquirer.prompt([{
+    name: "itemID",
+    type: "input",
+    message: "Please enter the item ID of the product you wish to purchase",
+    validate: function(value) {
+      if (isNaN(value) === false) {
+        return true;
+      }
+      return false;
+    }
+  }{
+    name: "quantity",
+    type: "input",
+    message: "Please enter the quantity of the product you wish to purchase",
+    validate: function(value) {
+      if (isNaN(value) === false) {
+        return true;
+      }
+      return false;
+    }
+  }]);
+}
