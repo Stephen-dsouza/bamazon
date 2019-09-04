0ca2485361c017024aeeb81691cae39d1fa639b2
Stephen <stephen.roy.dsouza@gmail.com>
2019-09-04 18:57:37 +1000

    create npm

---
+++ bamazon.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "srds1221",
+    database: "bamazon"
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+});
