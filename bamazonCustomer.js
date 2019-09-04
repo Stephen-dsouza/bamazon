027eed356e5651cb888fb77d72319b18d6b1439d
Stephen <stephen.roy.dsouza@gmail.com>
2019-09-04 19:17:25 +1000

    renamed bamazon.js to bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,4 +14,5 @@ var connection=mysql.createConnection((keys.sql));
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
+    
 });
