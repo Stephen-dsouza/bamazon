e50a7d4f874200266c96fb9329bbb516044e6fe0
Stephen D'Souza <stephen.roy.dsouza@gmail.com>
2019-09-06 08:25:34 +1000

    added logic to update remaining stock

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -52,31 +52,36 @@ function customerSelection() {
     ])
     .then(function (answer) {
       var ID = answer.itemID;
-      var columns = ["product_name", "stock_quantity"];
+      var columns = ["product_name", "stock_quantity","price"];
       var query = connection.query("SELECT ?? FROM products where item_id=?", [columns, ID], function (err, res) {
         if (err) throw err;
         console.log(answer.quantity);
         console.log(res[0].stock_quantity);
         if (answer.quantity > res[0].stock_quantity) {
-          console.log("Insuffiecient quantity available.We currently have " + res[0].stock_quantity + " available");
-
+          console.log("Insufficient quantity available.We currently have " + res[0].stock_quantity + " available");
           // console.log("valid" +res.stock_quantity);
         } else {
-          console.log("Invalid");
+          var remaningStock= res[0].stock_quantity-answer.quantity
+          connection.query("UPDATE products set ? where ?",
+            [{
+                stock_quantity: remaningStock
+              },
+              {
+                item_id: answer.itemID
+              }
+            ],
+            function (error) {
+              if (error) throw error;
+              var total = (answer.quantity * res[0].price);
+              console.log("Purchase successful!");
+              console.log("The total of your purchase is: $" + total);
+              // connection.end(res);
+              customerPurchase()
+            }
+          );
         }
-
-        connection.end(res);
-
-
+        
       });
-
-      // console.log(query.sql);
-
-      // var product=answer.itemID;
-
-      // console.log(product);
-      // if(answer.quantity>product.stock_quantity)
-      //   console.log("Sorry,Item" +product+" does not have the quanity you requested");
-      // });
     });
+    
 }
