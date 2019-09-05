4ee6f612b00f97eb006e3817173f2acbb67c04be
Stephen D'Souza <stephen.roy.dsouza@gmail.com>
2019-09-05 14:42:04 +1000

    Update bamazonCustomer.js
    working on checking quantity in db based on user input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,3 @@
-
 //for .env to save sql keys
 require("dotenv").config();
 var keys = require("./keys.js");
@@ -6,48 +5,69 @@ var keys = require("./keys.js");
 var inquirer = require("inquirer");
 //for mysql connection and npm package
 var mysql = require("mysql");
-var connection=mysql.createConnection((keys.sql));
+var connection = mysql.createConnection((keys.sql));
 
 
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    customerPurchase();
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  customerPurchase();
 });
 
-function customerPurchase(){
-  connection.query("SELECT * FROM products", function(err, res) {
+function customerPurchase() {
+  connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
     // Log all results of the SELECT statement
     console.table(res);
-    connection.end(res);
+    // connection.end(res);
     //Inquirer to prompt for product id and quantity
     customerSelection();
   });
-  
+
 }
 
-function customerSelection(){
+function customerSelection() {
   inquirer.prompt([{
-    name: "itemID",
-    type: "input",
-    message: "Please enter the item ID of the product you wish to purchase",
-    validate: function(value) {
-      if (isNaN(value) === false) {
-        return true;
+        name: "itemID",
+        type: "input",
+        message: "Please enter the item ID of the product you wish to purchase",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "Please enter the quantity of the product you wish to purchase",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
       }
-      return false;
-    }
-  },
-  {
-    name: "quantity",
-    type: "input",
-    message: "Please enter the quantity of the product you wish to purchase",
-    validate: function(value) {
-      if (isNaN(value) === false) {
-        return true;
-      }
-      return false;
-    }
-  }]);
+    ])
+    .then(function (answer) {
+      var ID = answer.itemID;
+      var columns = ["product_name", "stock_quantity"];
+      // connection.query("SELECT ?? FROM products where item_id=?",[columns,ID], function (err, res) {
+      connection.query("SELECT product_name,stock_quantity FROM products", function (err, res) {
+        if (err) throw err;
+
+        console.log(query.sql);
+        connection.end(res);
+      });
+
+      // console.log(query.sql);
+
+      // var product=answer.itemID;
+
+      // console.log(product);
+      // if(answer.quantity>product.stock_quantity)
+      //   console.log("Sorry,Item" +product+" does not have the quanity you requested");
+      // });
+    });
 }
