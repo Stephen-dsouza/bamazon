ac874cb7554365c924fb7834496ca8ff0c8a5586
Stephen D'Souza <stephen.roy.dsouza@gmail.com>
2019-09-06 13:32:04 +1000

    more info around what each function does added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,35 +7,35 @@ var inquirer = require("inquirer");
 var mysql = require("mysql");
 var connection = mysql.createConnection((keys.sql));
 
-
+// check connection to the db and run function start 
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
   start();
 });
 
-
+//This will prompt the customer if they wish to make a purchase or exit
 function start() {
   inquirer.prompt([{
-    name: "action",
-    type: "list",
-    choices: ["Purchase Item", "Exit"],
-    message: "please select if you wish to purchase or exit",
-  }])
-  .then(function(answer){
-     
-    switch (answer.action) {
-      case ("Purchase Item"):
-        customerPurchase();
-        break;
-      case ("Exit"):
-        connection.end();
-    }
+      name: "action",
+      type: "list",
+      choices: ["Purchase Item", "Exit"],
+      message: "please select if you wish to purchase or exit",
+    }])
+    .then(function (answer) {
 
-  });
+      switch (answer.action) {
+        case ("Purchase Item"):
+          customerPurchase();
+          break;
+        case ("Exit"):
+          connection.end();
+      }
+
+    });
 }
 
-// display the items
+// display the items in stock and prompt for selection
 function customerPurchase() {
   connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
@@ -74,12 +74,12 @@ function customerSelection() {
     .then(function (answer) {
       var ID = answer.itemID;
       var columns = ["product_name", "stock_quantity", "price"];
-       connection.query("SELECT ?? FROM products where item_id=?", [columns, ID], function (err, res) {
+      connection.query("SELECT ?? FROM products where item_id=?", [columns, ID], function (err, res) {
         if (err) throw err;
         if (answer.quantity > res[0].stock_quantity) {
           console.log("\n Insufficient quantity available.We currently have " + res[0].stock_quantity + " available \n");
           start();
-          
+
         } else {
           var remaningStock = res[0].stock_quantity - answer.quantity
           connection.query("UPDATE products set ? where ?",
@@ -94,13 +94,10 @@ function customerSelection() {
               if (error) throw error;
               var total = (answer.quantity * res[0].price);
               console.log("Purchase successful!");
-              console.log("The total of your purchase is: $" + total +"\n\n");
-              // connection.end(res);
               start()
             }
           );
         }
-
       });
     });
 
